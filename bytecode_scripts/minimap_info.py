8adfb3bc8c4472ffd58b6e800e15532c3f97eb71
Joseph Camacho <camacho.joseph@gmail.com>
2023-01-16 04:35:49 -0500

    Added a function to get nearest well (including current chunk) to MinimapInfo

--- bytecode_scripts/minimap_info.py
+++ bytecode_scripts/minimap_info.py
@@ -26,6 +26,7 @@ def make_nearest_enemy_chunk():
         funcs += "\n" + func
     return funcs
 
+
 WELL_BIT = 0b10
 def make_nearest_well_chunk():
     funcs = """public static int nearestWellChunk (int chunk, int[] chunks) {
@@ -35,7 +36,7 @@ def make_nearest_well_chunk():
     funcs += f"}}\nreturn nearestWellChunkSub0(chunks);}}"
     for id, chunk in enumerate(chunk_centers.keys()):
         center = chunk_centers[chunk]
-        chunks = sorted(chunk_centers.keys(), key=lambda c: float('inf') if c == chunk else r2(chunk_centers[c], center))
+        chunks = sorted(chunk_centers.keys(), key=lambda c: r2(chunk_centers[c], center))
         func = f"private static int nearestWellChunkSub{id} (int[] chunks) {{"
         for c in chunks:
             func += f"\nif ((chunks[{c}] & {WELL_BIT}) == 1) return {c};"
@@ -43,12 +44,32 @@ def make_nearest_well_chunk():
         funcs += "\n" + func
     return funcs
 
-s = f"""package firstbot.coordination;
+
+WELL_BIT = 0b10
+def make_nearest_well_chunk_other():
+    funcs = """public static int nearestWellChunkOther (int chunk, int[] chunks) {
+        switch(chunk) {"""
+    for id, chunk in enumerate(chunk_centers.keys()):
+        funcs += f"\ncase {chunk}: return nearestWellChunkOtherSub{id}(chunks);"
+    funcs += f"}}\nreturn nearestWellChunkOtherSub0(chunks);}}"
+    for id, chunk in enumerate(chunk_centers.keys()):
+        center = chunk_centers[chunk]
+        chunks = sorted(chunk_centers.keys(), key=lambda c: float('inf') if c == chunk else r2(chunk_centers[c], center))
+        func = f"private static int nearestWellChunkOtherSub{id} (int[] chunks) {{"
+        for c in chunks:
+            func += f"\nif ((chunks[{c}] & {WELL_BIT}) == 1) return {c};"
+        func += f"\nreturn -1;}}"
+        funcs += "\n" + func
+    return funcs
+
+s = f"""package secondbot.coordination;
 
 /* Finds out specific info about the minimap given the chunks array.
  * Current methods:
  * - nearestEnemyChunk(int chunkIndex) // Finds the nearest chunk with an enemy given your location's chunk index. Returns -1 if no known enemy chunks.
- * - nearestWellChunk(int chunkIndex) // Finds the nearest chunk with a well given your location's chunk index. Places your current chunk at the back of the line.
+ * - nearestWellChunk(int chunkIndex) // Finds the nearest chunk with a well given your location's chunk index. Places your current chunk at the front of the line.
+                                      // Returns -1 if no known well chunks.
+ * - nearestWellChunkOther(int chunkIndex) // Finds the nearest chunk with a well given your location's chunk index. Places your current chunk at the back of the line.
                                       // Returns -1 if no known well chunks.
 */
 
@@ -57,6 +78,7 @@ import battlecode.common.*;
 public class MinimapInfo {{
     {make_nearest_enemy_chunk()}
     {make_nearest_well_chunk()}
+    {make_nearest_well_chunk_other()}
 }}"""
 
 print(s)
